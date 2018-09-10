c422eb5bb31f48cdf1be92c1b636b378faf1cf9f
kamioteppei <kamioteppei@gmail.com>
2018-09-10 16:21:58 +0900

    some changes

--- _js/origin.js
+++ _js/origin.js
@@ -4,6 +4,9 @@ var smooziee = require('smooziee.js');
 var precode = require('precode.js');
 var motiv = require('motiv.scss');
 
+var jqxTree = require('jqxTree');
+
+
 $(function(){
   $('.toc-src').tocjs({
     min: 2
@@ -11,4 +14,13 @@ $(function(){
 
   $('pre').precode();
   $.smooziee();
+});
+
+$(function(){
+  $('.jqxTree').jqxTree({
+      height: '300px',
+      width: '300px',
+      theme: 'energyblue'
+
+  });
 });
